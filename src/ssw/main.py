ed7d63ad8414b4ef52c1c4198276adb94c32aa69
Shiyang Chen <117800206+Fil-end@users.noreply.github.com>
2024-07-19 14:22:18 +0800

    Add files via upload

--- src/ssw/main.py
+++ src/ssw/main.py
@@ -4,21 +4,27 @@ from ase.io.lasp_PdO import write_arc, read_arc
 
 def lasp_ssw_calc(atom, type):
     write_arc([atom])
-    if type == 's' or 'short':
-        atom.calc = LASP(task='short-global-opt', pot='PdO', potential='NN D3')
-    elif type == 'l' or 'long':
-        atom.calc = LASP(task='long-global-opt', pot='PdO', potential='NN D3')
+    if type in ['s', 'short']:
+        atom.calc = LASP(task='ssw', pot='PdO', potential='NN D3')
+    elif type in ['l', 'long']:
+        atom.calc = LASP(task='long-ssw', pot='PdO', potential='NN D3')
     energy = atom.get_potential_energy()
     force = atom.get_forces()
-    atom = read_arc('allstr.arc', index = -1)
+    atom = read_arc('best.arc', index = -1)
     return atom, energy, force
 
-if __name__ == '__main__':
-    energy_list = []
-    traj = []
+def postprocessing(env:MCTEnv):
+    atoms = read_arc('allstr.arc', index = 0)
+    atoms = env.del_env_adsorbates(atoms)
+    atoms, energy,_ = lasp_ssw_calc(atoms, 'l')
+    return atoms, energy
 
+if __name__ == '__main__':
     env = MCTEnv(save_dir = 'save_dir', save_every= 20 ,timesteps = 1000, reaction_H = 0.790, reaction_n = 70, 
-             delta_s = -0.414, model_path = 'PdO', calculator_method = 'lasp') 
+             delta_s = -0.414, use_GNN_description = True,model_path = 'PdO', calculator_method = 'lasp') 
+    '''energy_list = []
+    O_num_list = [0]
+    traj = []
 
     initial_slab = env._generate_initial_slab()
     slab, initial_energy, _ = env.calculator(initial_slab)
@@ -26,7 +32,7 @@ if __name__ == '__main__':
     energy_list.append(initial_energy)
     traj.append(slab.copy())
 
-    for _ in range(40):
+    for _ in range(70):
         slab = env.choose_ads_site(slab)
         env.to_constraint(slab)
         slab, energy, _ = lasp_ssw_calc(slab, 's')
@@ -35,9 +41,11 @@ if __name__ == '__main__':
         traj.append(slab.copy())
 
     best_atoms = traj[energy_list.index(min(energy_list))]
+    env.to_constraint(best_atoms)
     best_atoms, best_energy,_ = lasp_ssw_calc(best_atoms, 'l')
     best_energy += env.E_O2 * env.n_O2 + env.E_O3 * env.n_O3
     print(f'energy_list is: {energy_list}')
-    print(f'best_energy is: {best_energy}')
+    print(f'best_energy is: {best_energy}')'''
 
-    write_arc([best_atoms],  file_name = 'best_1.arc')
+    best_atoms, best_energy = postprocessing(env)
+    write_arc([best_atoms],  name = 'best_1.arc')
