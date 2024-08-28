62d5a7a7af307a433ba93802779f1815c469c1b0
Shiyang Chen <117800206+Fil-end@users.noreply.github.com>
2024-08-29 01:54:22 +0800

    Add files via upload

--- src/asop/postprocessing.py
+++ src/asop/postprocessing.py
@@ -1,9 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 from lasp import LASP
 
-from ase.build import fcc110, bulk
-from ase.io import read
+from ase import Atoms
+from ase.build import fcc110
+from ase.geometry.analysis import Analysis
+from ase.io import read, write
+from ase.io.dmol import read_dmol_arc, write_dmol_arc
 from asop import ASOP
 
 K = 8.617 * 1E-5
@@ -12,6 +16,25 @@ initial_slab = fcc110('Ag', size=(20, 20, 6), vacuum=10.0)
 mock_slab = fcc110('Ag', size = (1,1,6), vacuum = 10.0)
 Ag_bulk = read('Ag.poscar')
 Ag2O = read('Ag2O.poscar')
+
+def env_O2(slab:Atoms) -> Atoms:
+    ana = Analysis(slab)
+    OOBonds = ana.get_bonds('O', 'O', unique = True)
+    PdOBonds = ana.get_bonds('Ag', 'O', unique=True)
+
+    Pd_O_list = []
+    del_list = []
+
+    if PdOBonds[0]:
+        for PdO_bond in PdOBonds[0]:
+            Pd_O_list.extend([PdO_bond[0], PdO_bond[1]])
+    
+    if OOBonds[0]:
+        for OO in OOBonds[0]:
+            if OO[0] not in Pd_O_list and OO[1] not in Pd_O_list:
+                del_list.extend([OO[0], OO[1]])
+
+    return bool(del_list)
 
 def get_surface_energy(TM_2D):
     primitive_cell = mock_slab.cell
@@ -67,6 +90,7 @@ if __name__ == '__main__':
     A_list = []
     grids_list = []
     energy_list = []
+    arc_path_list = []
     initial_energy_list = []
 
     current_TM = None
@@ -77,7 +101,7 @@ if __name__ == '__main__':
         if os.path.exists(f'{dir}/asop.npz',):
             data = np.load(f'{dir}/asop.npz', allow_pickle = True)
 
-            i_e_list = []
+            # i_e_list = []
 
             miu = data['miu']
             Ag = data['Ag']
@@ -87,44 +111,88 @@ if __name__ == '__main__':
             grids = data['grids']
             energy = data['energy']
 
-            for matrix in A_matrix:
-                matrix = matrix.tolist()
-                if matrix != current_TM:
-                    current_TM = matrix
-                    current_initial_energy = get_surface_energy(current_TM)
-                    i_e_list.append(current_initial_energy)
-                else:
-                    i_e_list.append(current_initial_energy)
+            for idx in range(len(A_matrix)):
+                n_Ag = Ag[idx]
+                n_O = O[idx]
+                TM = A_matrix[idx]
 
-            Ag_list.extend(Ag)
-            O_list.extend(O)
-            A_matrix_list.extend(A_matrix)
-            A_list.extend(A)
-            grids_list.extend(grids)
-            energy_list.extend(energy - np.array(i_e_list))
+                u_p,v_p = TM
+                norm_u = np.round(np.linalg.norm(u_p), 3)
+                norm_v = np.round(np.linalg.norm(v_p), 3)
 
-    miu_list = (np.array(energy_list) - E_O * np.array(O_list) - E_Ag * np.array(Ag_list)) / np.array(A_list)
-    miu_list = miu_list.tolist()
+                arc_path_list.append(f"./{dir}/save_dir/{norm_u}x{norm_v}/Ag{n_Ag}O{n_O}.arc")
 
-    save_path = os.path.join('./', f"{'asop'}.npz")
-    np.savez_compressed(
-        save_path,
-        miu = miu_list,
-        energy = energy_list,
-        Ag = Ag_list,
-        O = O_list,
-        A_matrix = A_matrix_list,
-        A = A_list,
-        grids = grids_list,
-    )
+            # for matrix in A_matrix:
+            #     matrix = matrix.tolist()
+            #     if matrix != current_TM:
+            #         current_TM = matrix
+            #         print(current_TM)
+            #         current_initial_energy = get_surface_energy(current_TM)
+            #         i_e_list.append(current_initial_energy)
+            #     else:
+            #         i_e_list.append(current_initial_energy)
+
+            # Ag_list.extend(Ag)
+            # O_list.extend(O)
+            # A_matrix_list.extend(A_matrix)
+            # A_list.extend(A)
+            # grids_list.extend(grids)
+            # energy_list.extend(energy - np.array(i_e_list))
+
+    # miu_list = (np.array(energy_list) - E_O * np.array(O_list) - E_Ag * np.array(Ag_list)) / np.array(A_list)
+    # miu_list = miu_list.tolist()
+
+    # save_path = os.path.join('./', f"{'asop'}.npz")
+    # np.savez_compressed(
+    #     save_path,
+    #     miu = miu_list,
+    #     energy = energy_list,
+    #     Ag = Ag_list,
+    #     O = O_list,
+    #     A_matrix = A_matrix_list,
+    #     A = A_list,
+    #     grids = grids_list,
+    # )
+    
+    data = np.load(f'asop.npz', allow_pickle = True)
+    target_atoms = []
+    miu_list = data['miu'].tolist()
+    A_matrix_list = data['A_matrix'].tolist()
+    A_list = data['A']
+    Ag_list = data['Ag']
+    O_list = data['O']
 
     for min_idx in range(len(miu_list)):
-        if miu_list[min_idx] < -0.4:
-            # min_idx = miu_list.index(min(miu_list))
+        if miu_list[min_idx] < -0.3:
             n_Ag = Ag_list[min_idx]
             n_O = O_list[min_idx]
             TM = A_matrix_list[min_idx]
             A = A_list[min_idx]
+            A_matrix = A_matrix_list[min_idx]
+            arc_path = arc_path_list[min_idx]
 
-            # print(miu_list)
-            print(f"Logging Info: TM is {TM}, Ag is {n_Ag}, O is {n_O}, miu is {miu_list[min_idx]}")
+            atoms = read_dmol_arc(arc_path)
+            if not env_O2(atoms):
+                target_atoms.append(atoms)
+                print(f"Logging Info: TM is {TM}, Ag is {n_Ag}, O is {n_O}, miu is {miu_list[min_idx]}")
+    
+    # write_dmol_arc('best_traj.arc', target_atoms)
+    write('best.xyz', target_atoms)
+    
+    # 创建示例数据
+    # x = np.linspace(1, A, A)
+    # y = np.linspace(1, A, A)
+    # best_data = np.random.rand(A, A)  # 生成一个10x10的随机数据矩阵
+    # for idx in range(len(miu_list)):
+    #     if A_matrix_list[idx] == TM:
+    #         n_Ag = Ag_list[idx]
+    #         n_O = O_list[idx]
+    #         best_data[n_Ag - 1][n_O - 1] = miu_list[idx]
+    
+    # 创建值热力图
+    # plt.contourf(x, y, best_data, cmap='coolwarm', levels=20)
+    # plt.colorbar(label='miu')  # 添加颜色条
+    # plt.title(f'ASOP_{TM}')
+    # plt.xlabel('Ag Axis')
+    # plt.ylabel('O Axis')
+    # plt.savefig('asop.png')
